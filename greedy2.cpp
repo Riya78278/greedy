e4a9312b73cbe03beb65087c91290c52af45dd11
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-11-25 23:41:39 +0530

    Add files via upload

---
+++ greedy2.cpp
@@ -0,0 +1,34 @@
+// Shop in Candy Store
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> minMaxCandy(vector<int>& prices, int k) {
+        // Code here
+        //sort karke two pointer
+        sort(prices.begin(),prices.end());
+        int start = 0;
+        int low_cost = 0;
+        int high_cost= 0;
+        int end=  prices.size()-1;
+        while(start<=end){
+            int j = k;
+            low_cost +=  prices[start];
+            end -= j;
+            start++;
+        } 
+        start = 0 ;
+        end = prices.size()-1;
+        while(start<=end){
+            int j = k;
+            high_cost += prices[end];
+            start += j;
+            end--;
+        }
+        vector<int>a;
+        a.push_back(low_cost);
+        a.push_back(high_cost);
+        return a;
+    }
+};
