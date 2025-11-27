ccabe96af670b6d9575e8db0a383c781565dbcc9
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2025-11-28 00:13:39 +0530

    Add files via upload

---
+++ greedy6.cpp
@@ -0,0 +1,33 @@
+// Minimum Cost of ropes
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+    int minCost(vector<int>& arr) {
+        // code here
+        priority_queue<int, vector<int>, greater<int>> pq;  
+        
+        if(arr.size()==1){
+            return 0;
+        }
+        if(arr.size()==2){
+            return arr[0]+arr[1];
+        }
+        for(int i=0;i<arr.size();i++){
+            pq.push(arr[i]);
+        }
+        int ans=0;
+        while(pq.size()>=2){
+            int top=pq.top();
+            pq.pop();
+            int sectop=pq.top();
+            pq.pop();
+            int cost=top+sectop;
+            pq.push(cost);
+            ans=ans+cost;
+        }
+        
+        return ans;
+    }
+};
